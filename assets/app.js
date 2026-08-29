4e9d9913d881e9f5d693101fbfdca7eac5ab06f3
qk1234567870 <182346359+qk1234567870@users.noreply.github.com>
2026-08-29 23:10:11 +0800

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -76,7 +76,8 @@
     if (isNaN(d.getTime())) return "";
     try {
       return new Intl.DateTimeFormat("zh-TW", {
-        month:"numeric", day:"numeric", hour:"2-digit", minute:"2-digit", hour12:false
+        year:"numeric", month:"numeric", day:"numeric",
+        hour:"2-digit", minute:"2-digit", hour12:false
       }).format(d);
     } catch (e) { return ""; }
   }
