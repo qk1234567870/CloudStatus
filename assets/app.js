dc995a53bf49b94b7a68b1b4130c8100a890c252
qk1234567870 <182346359+qk1234567870@users.noreply.github.com>
2026-09-03 14:58:14 +0800

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -3,11 +3,11 @@
 
   function startApp() {
   var CONFIG = Object.freeze({
-    version: "64.0.0",
+    version: "66.0.0",
     expectedServiceCount: 23,
 
     refreshInterval: 5 * 60 * 1000,
-    cacheKey: "cloudstatus-cache-v64",
+    cacheKey: "cloudstatus-cache-v66",
     cacheMaxAge: 15 * 60 * 1000,
     staleCacheMaxAge: 24 * 60 * 60 * 1000,
     foregroundRefreshThreshold: 2 * 60 * 1000,
@@ -25,7 +25,7 @@
   var state = { services: [], filter: "all", search: "", activeOnly: false };
 
   var REFRESH_INTERVAL = CONFIG.refreshInterval || 5 * 60 * 1000;
-  var CACHE_KEY = CONFIG.cacheKey || "cloudstatus-cache-v64";
+  var CACHE_KEY = CONFIG.cacheKey || "cloudstatus-cache-v66";
   var CACHE_MAX_AGE = CONFIG.cacheMaxAge || 15 * 60 * 1000;
   var STALE_CACHE_MAX_AGE = CONFIG.staleCacheMaxAge || 24 * 60 * 60 * 1000;
   var FETCH_TIMEOUT = CONFIG.fetchTimeout || 6500;
@@ -1293,8 +1293,13 @@
   function render() {
     renderSummary();
     var list=visibleServices();
+    var cardTemplate=window.CloudStatusCardTemplate;
+    if(!cardTemplate || typeof cardTemplate.render!=="function"){
+      throw new Error("CloudStatus card template is not loaded");
+    }
+    var ctx=templateContext();
     $("#services").innerHTML=list.length
-      ? CloudStatusRenderer.renderServices(list,templateContext())
+      ? list.map(function(service){return cardTemplate.render(service,ctx);}).join("")
       : '<div class="empty">沒有符合條件的服務或事件。</div>';
     layoutDesktopMasonry();
   }
--- assets/services.js
+++ assets/services.js
@@ -2,7 +2,7 @@
 (function () {
   "use strict";
 
-  var version = "64.0.0";
+  var version = "66.0.0";
   var expectedServiceCount = 23;
 
   var manifest = [
