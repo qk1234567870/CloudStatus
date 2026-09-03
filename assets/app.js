b4af2f15bfad1fe471eb7bb626ca0f861ed43eea
qk1234567870 <182346359+qk1234567870@users.noreply.github.com>
2026-09-03 16:40:55 +0800

    更新 app.js

--- assets/app.js
+++ assets/app.js
@@ -17,7 +17,7 @@
     fallbackConcurrency: 4,
 
     desktopMasonryMinWidth: 760,
-    desktopMaxWidth: 1100,
+    desktopMaxWidth: 1180,
     masonryGap: 14
     });
   var SERVICES = window.CLOUDSTATUS_SERVICES || [];
