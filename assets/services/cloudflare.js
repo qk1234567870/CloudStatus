0a6f6b8a7978b1586b07f73c192d1e091449fdc3
qk1234567870 <182346359+qk1234567870@users.noreply.github.com>
2026-09-05 06:57:24 +0800

    Add files via upload

--- assets/services/cloudflare.js
+++ assets/services/cloudflare.js
@@ -13,7 +13,6 @@
     {
       "type": "statuspage",
       "url": "https://www.cloudflarestatus.com/api/v2/incidents.json",
-      "unresolvedUrl": "https://www.cloudflarestatus.com/api/v2/incidents/unresolved.json",
       "label": "官方 API",
       "tier": 10,
       "kind": "official-api",
